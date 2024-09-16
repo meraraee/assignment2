7ae01843f2e8bc50a438b446de4254f35d1d9a1f
Merary Murillo <meramrqz7@gmail.com>
2024-09-15 20:00:10 -0700

    Add files via upload

---
+++ fall2024/activity05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //to find the size, in bytes, of a data type we use sizeof()
+    
+    //outputting the size of a short and double
+    
+    cout << "Size of a short: " << sizeof(short) << endl;
+    cout << "Size of a long: " << sizeof(long) << endl;
+    
+    
+    //P.S. we dont necessarily need to declare a variable and assign a value
+    
+    
+    
+    return 0;
+}
