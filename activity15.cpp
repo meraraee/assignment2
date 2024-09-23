e388b9fe066f4d4fe1df9d06769962427fb87a6f
Merary Murillo <meramrqz7@gmail.com>
2024-09-23 11:36:42 -0700

    Add files via upload

---
+++ activity15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int rows;
+    
+    cout << "enter the number of rows: ";
+    cin >> rows;
+    
+    for(int i = 0; i <= rows; i++){
+        
+        for(int j = 1; j <= i; j++){
+            
+            cout << "*";
+        }
+        cout << endl;
+        
+    }
+    
+    
+    return 0;
+}
